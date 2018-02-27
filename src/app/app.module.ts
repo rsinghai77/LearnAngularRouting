443c9c38b14c30ca99bc6ce851e00bbd609e2dfd
Ravi Singhai <ravi.singhai@adtran.com>
2018-02-27 16:04:17 -0600

    Added hero module with its own routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule }     from './heroes/heroes.module';
 
 import { CrisisListComponent }   from './crisis-list.component';
-import { HeroListComponent }     from './hero-list.component';
 import { PageNotFoundComponent } from './not-found.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HeroesModule,
     AppRoutingModule
   ],
   declarations: [
     AppComponent,
-    HeroListComponent,
     CrisisListComponent,
     PageNotFoundComponent
   ],
