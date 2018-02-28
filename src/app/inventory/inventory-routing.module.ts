3c58c859fb9d39d7bdeadcffb0e809d2dda05f4d
Ravi Singhai <ravi.singhai@adtran.com>
2018-02-28 11:15:43 -0600

    Using feature child route format

--- src/app/inventory/inventory-routing.module.ts
+++ src/app/inventory/inventory-routing.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { InventoryComponent } from './inventory.component';
 import { NetworkInvComponent } from './network-inv.component';
 import { ServiceInvComponent } from './service-inv.component';
 import { InfrastructureInvComponent } from './infrastructure-inv.component';
 
-const inventoryRoutes: Routes = [
+const inventoryRoutesx: Routes = [
   { path: 'inventory/network', component: NetworkInvComponent },
   { path: 'inventory/service', component: ServiceInvComponent },
   { path: 'inventory/infrastructure', component: InfrastructureInvComponent }
+];
+
+const inventoryRoutes: Routes = [
+  {
+    path: 'inventory', component: InventoryComponent,
+    children: [
+      { path: 'network', component: NetworkInvComponent },
+      { path: 'service', component: ServiceInvComponent },
+      { path: 'infrastructure', component: InfrastructureInvComponent },
+      { path: '', component: NetworkInvComponent }
+    ]
+  }
 ];
 
 @NgModule({
--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -2,6 +2,7 @@ import { NgModule }       from '@angular/core';
 import { CommonModule }   from '@angular/common';
 import { FormsModule }    from '@angular/forms';
 
+import { InventoryComponent } from './inventory.component';
 import { NetworkInvComponent } from './network-inv.component';
 import { ServiceInvComponent } from './service-inv.component';
 import { InfrastructureInvComponent } from './infrastructure-inv.component';
@@ -16,6 +17,7 @@ import { InventoryService } from './inventory.service';
     InventoryRoutingModule
   ],
   declarations: [
+    InventoryComponent,
     NetworkInvComponent,
     ServiceInvComponent,
     InfrastructureInvComponent
